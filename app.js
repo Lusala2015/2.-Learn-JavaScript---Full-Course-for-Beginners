927641b68e3cdba1b0e122b28ab9af8d61df2ddc
Lusala2015 <lusalamark17@gmail.com>
2020-05-31 15:37:20 +0300

    Introduction

---
+++ app.js
@@ -0,0 +1,6 @@
+//Download a code editor
+//Decide on your favourite code editor to download; 
+//  Have Chrome browser installed in order to access developer tools or press F12 on the keyboard 
+// To print Hello world on the console 
+
+console.log("Hello World !");
