ef76f7293c68640b45092be08b2b5aab5331547c
Lusala2015 <lusalamark17@gmail.com>
2020-05-31 15:44:19 +0300

    Data types and Variables in Javascript

---
+++ 3.0 datatyppes&variables.js
@@ -0,0 +1,11 @@
+// Data Types and Variables 
+ /*1. undefined -- something that has not been defined
+ 2. null --means nothing
+ 3. boolean---- true or false
+ 4. string -- statements
+ 5.symbol--- an immutable primitive value that is unique
+ 6. number--- means a number
+ 7.object-- can store a lot of different key value pairs 
+ Variables -- allows a computer to store data 
+ To declare a variable use a var key word
+ i.e var myName = "Mar";*/
