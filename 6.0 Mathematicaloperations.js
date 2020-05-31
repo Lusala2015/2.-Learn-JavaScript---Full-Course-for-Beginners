0a83396a1311bbb0679115e4029b4a286f9152b5
Lusala2015 <lusalamark17@gmail.com>
2020-05-31 15:57:24 +0300

    performing mathematical operations in Javascript

---
+++ 6.0 Mathematicaloperations.js
@@ -0,0 +1,68 @@
+  //perfotming mathematical operations on numbers in JavaScript
+  //Addition 
+  var sum = 20+20;
+  console.log(sum);
+ //subtraction
+  var subtraction = 20-20;
+  console.log(subtraction);
+  //Division 
+  var division = 20/20;
+  console.log(division);
+ //multiplication 
+  var multiplication = 20*20;
+  console.log(multiplication);
+  //remainder 
+  var remainder = 23%20;
+  console.log(remainder);
+ //Incrreamenting a number
+       var myVar = 58;
+       myVar++;
+    console.log(myVar); 
+  //Decrreamenting a number
+      var myVar = 58;
+      myVar--;
+   console.log(myVar);
+
+
+// compound assignment with augmented addition
+var a =4;
+var b =3;
+var c=5;
+ a+=45;
+b+=5;
+c+=6;
+console.log(a);
+console.log(b);
+console.log(c);
+// compound assignment with augmented subtraction
+var a =4;
+var b =3;
+var c=5;
+ a-=45;
+b-=5;
+c-=6;
+console.log(a);
+console.log(b); 
+console.log(c);
+// compound assignment with augmented multiplication
+var a =4;
+var b =3;
+var c=5;
+ a*=45;
+b*=5;
+c*=6;
+console.log(a);
+console.log(b);
+console.log(c);
+ 
+ 
+// compound assignment with augmenteddivision
+ var a =4;
+var b =3;
+var c=5;
+ a/=45;
+b/=5;
+c/=6;
+console.log(a);
+console.log(b);
+console.log(c);
