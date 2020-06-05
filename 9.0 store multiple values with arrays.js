06e3c4bf617183ec55244497e8e438849af6a74b
Lusala2015 <lusalamark17@gmail.com>
2020-06-05 21:32:55 +0300

    access arrays data with index

--- 9.0 store multiple values with arrays.js
+++ 9.0 store multiple values with arrays.js
@@ -5,3 +5,9 @@ var myArray = ["Quincy", 1];
 //Nested Arrays
 urArray = [ ["rk", 96], ["Mrk", 196] ,"Mark", 1996];
 
+//Access arrays data with index 
+var marksScored = [60, 70,80,90];
+
+var firstStudentScoreontheScoreshit = marksScored[0];
+
+console.log(firstStudentScoreontheScoreshit);
