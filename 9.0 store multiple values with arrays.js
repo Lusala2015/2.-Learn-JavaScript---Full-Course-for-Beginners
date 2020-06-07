2cb34f53358ee22d6604a3e2e75484d1fd88aac6
Lusala2015 <lusalamark17@gmail.com>
2020-06-07 08:38:24 +0300

    correcting errors

--- 9.0 store multiple values with arrays.js
+++ 9.0 store multiple values with arrays.js
@@ -22,8 +22,8 @@ console.log(myData);
 //Accessing multiple-Dimensional arrays with indexes
 
 // set up
-var myarray = [[1,2,3], [4,5,6],[7,8,9],[[10,22,23],[12,45,67,89,34],[3,6,4,8,9]]]
+var myArray = [[1,2,3], [4,5,6],[7,8,9],[[10,22,23],[12,45,67,89,34],[3,6,4,8,9]]]
 
 // only change the code below 
-var myData = myArray[0][0]
-console.log(myArray);
+var myData = myArray[2][1]
+console.log(myData);
